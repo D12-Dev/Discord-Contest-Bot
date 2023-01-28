0419a27e917c19c2710bd0fbdfe54c84d46b73c3
D12-hehe <jamie.snowman123@gmail.com>
2023-01-28 19:20:49 +0000

    Updated to new version

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.PREFIX = ".";
-exports.TOKEN = process.env.TOKEN 
-exports.MongoUrl = process.env.MONGOURL 
+exports.TOKEN = process.env.TOKEN /// You can change this with your token or add it as a env variable on host such as heroku
+exports.MongoUrl = process.env.MONGOURL /// You can change this with your db url or add it as a env variable on host such as heroku
